81aaa28362d23e9c8e8a63ec3441f00be50bea72
cjie9759 <cjie9759@qq.com>
2023-03-12 12:51:04 +0800

    build sh

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,5 @@
 set -e
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -trimpath -ldflags "-w -s"  .
 ssh ten "systemctl stop cj_hs.service"
 scp  ./hostListen ten:/root/hostls
 ssh ten "systemctl restart cj_hs.service"
