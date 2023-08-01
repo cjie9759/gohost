199b2195af968b28e094e6a25fd1a83fcfb7a93f
cjie9759 <cjie9759@qq.com>
2023-08-01 22:05:36 +0800

    fix docker build

--- docker_push.sh
+++ docker_push.sh
@@ -1,5 +1,7 @@
+set -e
 tag=$(date "+%y.%m.%d.%s")
-docker build . --tag docker.io/cjie9759/gohost:$tag
-docker build . --tag docker.io/cjie9759/gohost:latest
-docker push docker.io/cjie9759/gohost:$tag
-docker push docker.io/cjie9759/gohost:latest
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -trimpath -ldflags "-w -s"  .
+podman build . --tag docker.io/cjie9759/gohost:$tag
+podman build . --tag docker.io/cjie9759/gohost:latest
+podman push docker.io/cjie9759/gohost:$tag
+podman push docker.io/cjie9759/gohost:latest
